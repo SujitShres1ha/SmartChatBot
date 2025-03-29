1b140ecce20d26701f7ac1d868b7fb9f72de1781
Terrence McCarthy <terrencemccarthysnkrs@gmail.com>
2025-03-29 14:45:04 -0500

    force response to be arlington based

--- src/lib/gemini.js
+++ src/lib/gemini.js
@@ -6,9 +6,11 @@ const ai = new GoogleGenAI({ apiKey });
 
 export default async function gemini(query) {
 
+  const locationContext = `Imagine you are an expert living in Arlington, Texas. Do not say you are an arlington resident and keep your answers to a few sentences. Answer the following questions from the perspective of someone in arlington: \n\n ${query}`;
+
   const response = await ai.models.generateContent({
     model: "gemini-2.0-flash",
-    contents: [{ text: query }],
+    contents: [{ text: locationContext }],
   });
   console.log();
   return response.candidates[0].content.parts[0].text;
